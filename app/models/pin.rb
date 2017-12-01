52ef4e32e64280589bbd667edd88872107ca292c
RISHABH SHARMA <rishabhsharma@Rishabhs-MacBook-Pro.local>
2017-12-01 00:11:24 -0500

    App completed

---
+++ app/models/pin.rb
@@ -0,0 +1,8 @@
+class Pin < ApplicationRecord
+	require 'rmagick'
+	acts_as_votable
+	belongs_to :user
+
+	has_attached_file :image, styles: { medium: "100x100>"}
+  	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+end
